7e829ce468b9780b00e819a606df6056d8d238d5
Lawzrr <Rafik_uchiwa@hotmail.com>
2022-02-24 21:32:33 +0100

    Update secante.py

--- secante.py
+++ secante.py
@@ -2,16 +2,16 @@ import numpy as np
 import matplotlib.pyplot as plt 
 def secante(f, x0, x1, tol):
     iteration_max=1
-    a=f(x0)
-    b=f(x1)
     while iteration_max<100:
         iteration_max+=1
-        if b==a:
+        a=f(x0)
+        b=f(x1)
+        if b==a :
             return'error',1
-         x_new=x1-b*(x1-x0)/(b-a)
-        if abs(x_new-x1)< tol :
+        if abs(b-a)< tol :
             return x1,0
         else:
+            x_new=x1-b*(x1-x0)/(b-a)
             x0,x1=x1,x_new
     return 'error',-1
 
