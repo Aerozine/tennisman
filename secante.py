d030ecef66bd3235e4983a2238a6e9af4cc915af
Lawzrr <Rafik_uchiwa@hotmail.com>
2022-02-25 14:09:00 +0100

    kjfkjsqkjfd

--- secante.py
+++ secante.py
@@ -2,9 +2,9 @@ import numpy as np
 import matplotlib.pyplot as plt 
 def secante(f, x0, x1, tol):
     iteration_max=0
+    a=f(x0)
+    b=f(x1)
     while iteration_max<100:
-        a=f(x0)
-        b=f(x1)
         if b==a :
             return'error',1
         if abs(b)< tol :
@@ -13,4 +13,7 @@ def secante(f, x0, x1, tol):
             x_new=x1-b*(x1-x0)/(b-a)
             x0,x1=x1,x_new
         iteration_max+=1
+        a=b
+        b=f(x1)
     return 'error',-1
+
--- testbissction.py
+++ testbissction.py
@@ -1,14 +1,13 @@
-import bissection
+import secante
 import numpy as np
 from matplotlib import pyplot as plt
 from RechercheRacine import fun 
 from numpy.polynomial import Polynomial as poly 
 import random
-x=np.linspace(-10,10,150)
 
 def checkifgood(tol,roo,b):
     for i in range(0,len(roo)):
-        if (abs(roo[i]-b)<=tol*10):
+        if(abs(roo[i] - b)<=tol*10):
             return 1
     print(tol , roo, b ) 
 
@@ -28,13 +27,13 @@ for i in range (1,1000):
     r=np.roots(c)
     #print(b)
     f=poly(c)
-    plt.plot(x,f(x),"g")
     roo=[]
     array=f.roots()
     for i in range (0,len(array)):
         if (array[i].imag < 1e-5):
             roo.append(array[i].real) 
-    b=bissection.bissection(poly(c),roo[0]-1000*random.random(),roo[0]+1000*random.random(),tol)
+#    b=bissection.bissection(poly(c),roo[0]-1000*random.random(),roo[0]+1000*random.random(),tol)
+    b=secante.secante(poly(c),roo[0]-1000*random.random(),roo[0]+1000*random.random(),tol)
     #print(roo)
     checkifgood(tol,roo,b)
 #plt.plot(roo , np.zeros_like(roo),"or")
