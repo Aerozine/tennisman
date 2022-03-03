7f2672b3ad43f498ab62b3e7758661804b1126f5
Loic-Delbarre <Zhf3PzewgSoGtzlofybR@protonmail.com>
2022-03-03 21:39:16 +0100

    fin milestone

--- RechercheRacine/__main__.py
+++
@@ -1,5 +0,0 @@
-
-#def secante(f,x0,x1,tol):
-
-def bissection(f,x0,x1,tol):
-
--- bissection.py
+++ bissection.py
@@ -1,3 +1,4 @@
+import sys 
 import numpy as np 
 import matplotlib.pyplot as plt 
 from numpy.polynomial import Polynomial as poly 
@@ -20,18 +21,34 @@ from numpy.polynomial import Polynomial as poly
             return c
 """
 def bissection(f,x0,x1,tol):
+    a = x0 
+    b = x1
     fx0=f(x0)
     fx1=f(x1)
+    if(fx0):
+        return x0
+    if(fx1):
+        return x1
+    if(fx1>fx0)
+        return 1
+    if((fx1>0 and fx0>0)or(fx1<0 and fx0<0))
+        return -1
+# error -1 si la racine n existe pas 
+#1 si il ne sait pas utiliser la fonction 
+# pour vérifier si la fonction converge vers une racine , il suffit de regarder si l absice de la fonction va continuer a décroitre ie que forall p , q f(p) < f(q)
     while(abs(fx0-fx1)>tol):
-    #    print("test")
         tmp = (x0+x1)/2 
         ftmp=f(tmp)
+        if(latest==a or latest==b):
+            return 
         if(ftmp<0):
             x0=tmp
             fx0=ftmp
+            latest=x0
         elif(ftmp>0):
             x1=tmp
             fx1=ftmp
+            latest=x1
         else:
             return tmp
-    return(x0)
+    return(latest)
