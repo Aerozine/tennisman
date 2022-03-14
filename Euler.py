7739970455e013f119cf3f44a3a1d7647e2fb58b
Loic-Delbarre <Zhf3PzewgSoGtzlofybR@protonmail.com>
2022-03-14 12:27:04 +0100

    reecriture de Euler

--- Euler.py
+++ Euler.py
@@ -1,7 +1,8 @@
 import numpy as np 
-import const
+import const as c
+from scipy.integrate import solve_ivp
 step = 0.01
-def euler(t,y,f)
+def euler(t,y,f):
     yt=[y]
     for i in range(int(t/step)):
        dy=f(t,y)
@@ -15,7 +16,13 @@ def oderhs(t,y):
     p=np.array(y[:3])
     v=np.array(y[3:6])
     w=np.array(y[6:9])
-    Fm=cm*c*np.linalg.norm(v)**2*np.cross(v,w)/np.cross(v,w)
-    Ft=cd*c*np.linalg.norm(v)*v
-    a=Fd+Ft+Fm
-    return np.concatenate((v,a,alpa)) 
+    Fm=c.cm*c.c*np.linalg.norm(v)**2*np.cross(v,w)/np.cross(v,w)
+    Ft=c.cd*c.c*np.linalg.norm(v)*v
+    a=c.Fd+Ft+Fm
+    alpha=[0,0,0]
+    return np.concatenate((v,a,alpha)) 
+def event(t,y):
+    return 0<y[2]
+event.terminal = True
+y=[10,10,10,5,5,5,0,4,3]
+print(solve_ivp(oderhs,[0,10],y,events=event))
--- const.py
+++ const.py
@@ -19,3 +19,13 @@ hf=1
 g=-9.81 
  #coefficientderestitution [e] 0.5 0.8
 e=0.5
+# calcul de constante utile au calcul 
+import numpy as np 
+Fd = np.array([0,0,g])
+c=(rho*np.pi*d**2)/8*m
+if(normw := np.linalg.norm(w)):
+    cm=1/(2+1.96/(d*normw))
+#Fm=cm*c*np.linalg.norm(v)**2*np.cross(v,w)
+else:
+    cm=0
+
